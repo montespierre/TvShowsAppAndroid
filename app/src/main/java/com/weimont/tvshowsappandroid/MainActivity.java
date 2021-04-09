5e07fb5a8ef56fa283f844b4f9a275df0682865c
unknown <montespierre@gmail.com>
2021-04-08 22:56:44 -0500

    MVVM configuration

---
+++ app/src/main/java/com/weimont/tvshowsappandroid/MainActivity.java
@@ -0,0 +1,16 @@
+package com.weimont.tvshowsappandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    // TV Shows API
+    // https://www.episodate.com/api
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
